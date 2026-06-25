ee9428278f27fc1480c7a8e1ad9c1b4de1b69321
kevin zarama <kevin.zarama@pragma.com.co>
2026-06-25 13:18:53 -0500

    Fase 0: Configuración del Proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+javascript
+import './App.css';
+
+function App() {
+return (
+<div className="App">
+<h1>¡Bienvenido a Pragma React App!</h1>
+<p>Tu proyecto está funcionando correctamente.</p>
+</div>
+);
+}
+
+export default App;
