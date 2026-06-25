97833b7805c05374e697161b6cce5d253c31f66f
kevin zarama <kevin.zarama@pragma.com.co>
2026-06-25 15:29:43 -0500

    fase 3: Gestión de Estado Centralizada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Button from '../components/Button.tsx';
 import { useAppContext } from '../hooks/useAppContext.tsx';
+import { useAppStore } from '../store/store.js';
 
 const Home: React.FC = () => {
   const { count, increment } = useAppContext();
+  const user = useAppStore((state: { user: any; }) => state.user);
+  const setUser = useAppStore((state: { setUser: any; }) => state.setUser);
 
   return (
     <div>
       <h1>Home Page</h1>
       <p>Count: {count}</p>
       <Button onClick={increment}>Increment</Button>
+      
+      <p>Count: {user}</p>
+      <Button onClick={()=>setUser("new user")}>Reset</Button>
     </div>
   );
 };
